60aeeb0ccea154ff11b1ee6d438aed4018d53618
Terry.D <softwaltz@gmail.com>
2023-06-25 23:49:55 +0800

    modify main.go

--- main.go
+++ main.go
@@ -8,20 +8,7 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
-	"time"
 )
-
-const (
-	MAXCLIENTS  = 300
-	IDLETIMEOUT = 300
-)
-
-type client struct {
-	inuse    bool
-	inSock   net.Conn
-	outSock  net.Conn
-	activity time.Time
-}
 
 func main() {
 	if len(os.Args) == 1 {
@@ -38,7 +25,7 @@ func main() {
 			laddr := tmp[0] + ":" + tmp[1]
 			raddr := tmp[2] + ":" + tmp[3]
 			fmt.Println(i, ":", laddr, raddr)
-			go pipeLine2(laddr, raddr)
+			go pipeLine(laddr, raddr)
 		}
 	}
 
@@ -49,9 +36,8 @@ func main() {
 	fmt.Println("Stop Piping!!!")
 }
 
-func pipeLine2(listenAddr, remoteAddr string) {
+func pipeLine(listenAddr, remoteAddr string) {
 	fmt.Println(listenAddr, "<->", remoteAddr)
-
 	lsock, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		fmt.Println("Failed to listen:", err)
@@ -59,59 +45,27 @@ func pipeLine2(listenAddr, remoteAddr string) {
 	}
 	defer lsock.Close()
 
-	clients := make([]client, MAXCLIENTS)
+	copyIO := func(src, dest net.Conn) {
+		defer src.Close()
+		defer dest.Close()
+		io.Copy(src, dest)
+	}
 
 	for {
-		inSock, err := lsock.Accept()
+		conn, err := lsock.Accept()
 		if err != nil {
 			fmt.Println("Failed to accept connection:", err)
 			continue
 		}
 
-		i := findFreeClient(clients)
-		if i < 0 {
-			fmt.Println("Too many clients")
-			inSock.Close()
-			continue
-		}
-
-		outSock, err := net.Dial("tcp", remoteAddr)
+		proxy, err := net.Dial("tcp", remoteAddr)
 		if err != nil {
 			fmt.Println("Failed to connect to remote host:", err)
-			inSock.Close()
+			conn.Close()
 			continue
 		}
 
-		clients[i] = client{
-			inuse:    true,
-			inSock:   inSock,
-			outSock:  outSock,
-			activity: time.Now(),
-		}
-
-		handleClient(&clients[i])
+		go copyIO(conn, proxy)
+		go copyIO(proxy, conn)
 	}
-}
-
-func handleClient(cli *client) {
-	copyIO := func(src, dst net.Conn) {
-		src.SetReadDeadline(time.Now().Add(IDLETIMEOUT * time.Second))
-		dst.SetWriteDeadline(time.Now().Add(IDLETIMEOUT * time.Second))
-		defer src.Close()
-		defer dst.Close()
-		io.Copy(src, dst)
-	}
-
-	cli.activity = time.Now()
-	go copyIO(cli.inSock, cli.outSock)
-	go copyIO(cli.outSock, cli.inSock)
-}
-
-func findFreeClient(clients []client) int {
-	for i, cli := range clients {
-		if !cli.inuse {
-			return i
-		}
-	}
-	return -1
 }
